b99892810e84765b9f295344d3f127ad0dd51574
Max! <admin@cloudydev.net>
2025-06-03 20:28:46 +0200

    Update br-forward.sh

--- rke2/networkAttachmentDefinitions/br-forward.sh
+++ rke2/networkAttachmentDefinitions/br-forward.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# save to /usr/local/sbin/br-forawrd.sh
+# save to /usr/local/sbin/br-forward.sh
 # make sure to mark it executable:
-#  chmod u+x /usr/local/sbin/br-forawrd.sh
+#  chmod u+x /usr/local/sbin/br-forward.sh
 sudo iptables -F FORWARD && \
 sudo iptables -I FORWARD -m physdev --physdev-is-bridged -j ACCEPT && \
 sudo sysctl -w net.ipv4.ip_forward=1
